d4e3473ab4c5839e7c4c462141586a532ff8173c
soudeepan <soudeepanbiswas@gmail.com>
2024-01-04 20:37:06 +0530

    edited framework

---
+++ src/script_analysis.js
@@ -0,0 +1,56 @@
+import {dateMap,timeMap,senderMap,emojiMap,textMap} from "./data_arrangement.js";
+
+
+
+
+
+//no. of Messages send by the two persons
+
+
+
+messageMap = new Map(senderMap)
+
+const messageEntry1 = messageMap.entries().next().value;
+const messagePerson1 = messageEntry1[0];//name of person 1
+const messageValue1 = messageEntry1[1];//no. of messages by person 1
+
+const messageEntry2 = messageMap.entries().next().value;
+const messagePerson2 = messageEntry2[0];//name of person 2
+const messageValue2 = messageEntry2[1];//no. of messages by person 2
+
+
+
+
+
+// top 5 used emojis
+
+
+
+
+mostUsedEmojiMap = new Map(emojiMap)
+
+
+const emojiEntry1 = mostUsedEmojiMap.entries().next().value;
+const emoji1 = emojiEntry1[0]; // emoji 1
+const emojiValue1 = emojiEntry1[1]; // no. of times emoji 1 is used
+
+const emojiEntry2 = mostUsedEmojiMap.entries().next().value;
+const emoji2 = emojiEntry2[0]; // emoji 2
+const emojiValue2 = emojiEntry2[1]; // no. of times emoji 2 is used
+
+const emojiEntry3 = mostUsedEmojiMap.entries().next().value;
+const emoji3 = emojiEntry3[0]; // emoji 3
+const emojiValue3 = emojiEntry3[1]; // no. of times emoji 3 is used
+
+const emojiEntry4 = mostUsedEmojiMap.entries().next().value;
+const emoji4 = emojiEntry4[0]; // emoji 4
+const emojiValue4 = emojiEntry4[1]; // no. of times emoji 4 is used
+
+const emojiEntry5 = mostUsedEmojiMap.entries().next().value;
+const emoji5 = emojiEntry5[0]; // emoji 5
+const emojiValue5 = emojiEntry5[1]; // no. of times emoji 5 is used
+
+
+
+
+
